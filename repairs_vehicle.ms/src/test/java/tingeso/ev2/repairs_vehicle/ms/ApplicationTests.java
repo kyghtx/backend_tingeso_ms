0f1a397fbdcabb0f3eecce25642153a52af041a2
Nicolas Espina <nicolas.cartes@usach.cl>
2024-05-18 21:06:29 -0400

    inicio.

---
+++ repairs_vehicle.ms/src/test/java/tingeso/ev2/repairs_vehicle/ms/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tingeso.ev2.repairs_vehicle.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ reports_uh.ms/src/test/java/tingeso/ev2/reports_uh/ms/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tingeso.ev2.reports_uh.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

5bfc494f7cbb18eefc74a6c8972d4cc379b0f09a
Nicolas <83936791+kyghtx@users.noreply.github.com>
2025-07-05 04:12:48 -0400

    Update GeneralNavBar.jsx

--- frontend-tingeso-ms/src/components/GeneralNavBar.jsx
+++ frontend-tingeso-ms/src/components/GeneralNavBar.jsx
@@ -5,22 +5,22 @@ export function GeneralNavBar(){
     return(
         <nav className="NavBar">
           <Button className="nav-item" variant="outlined" color="secondary" size="large">
-            <Link className="nav-link" to="/api/vehicles">
+            <Link className="nav-link" to="/vehicles">
               Gestion Vehiculos
             </Link>
           </Button>
           <Button className="nav-item" variant="outlined" color="secondary" size="large">
-            <Link className="nav-link" to="/api/repairs_types">
+            <Link className="nav-link" to="/repairs_types">
               Gestion de Reparaciones
             </Link>
           </Button>
           <Button className="nav-item" variant="outlined" color="secondary" size="large">
-            <Link className="nav-link" to="/api/repair_vehicles/create">
+            <Link className="nav-link" to="/repair_vehicles/create">
               Ingresar Reparaciones
             </Link>
           </Button>
           <Button className="nav-item" variant="outlined" color="secondary" size="large">
-            <Link className="nav-link" to="/api/reports">
+            <Link className="nav-link" to="/reports">
               Reportes
             </Link>
           </Button>

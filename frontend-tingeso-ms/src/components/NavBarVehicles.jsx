e47e65d813b839ab43946a2d2d085272afd54905
Nicolas <83936791+kyghtx@users.noreply.github.com>
2025-07-05 05:02:06 -0400

    Update NavBarVehicles.jsx

--- frontend-tingeso-ms/src/components/NavBarVehicles.jsx
+++ frontend-tingeso-ms/src/components/NavBarVehicles.jsx
@@ -4,22 +4,22 @@ import { Link } from "react-router-dom";
 function NavBarVehicles() {
   return <nav className="NavBarVehicles">
           <Button className="nav-item">
-            <Link className="nav-link" to="/api/vehicles/Brands">
+            <Link className="nav-link" to="/vehicles/Brands">
               Marcas
             </Link>
           </Button>
           <Button className="nav-item">
-            <Link className="nav-link" to="/api/vehicles/Types">
+            <Link className="nav-link" to="/vehicles/Types">
               Tipos de vehiculos
             </Link>
           </Button>
           <Button className="nav-item">
-            <Link className="nav-link" to="/api/vehicles/MotorTypes">
+            <Link className="nav-link" to="/vehicles/MotorTypes">
               Tipos de motor
             </Link>
           </Button>
           <Button className="nav-item">
-            <Link className="nav-link" to="/api/vehicles">
+            <Link className="nav-link" to="/vehicles">
               Vehiculos
             </Link>
           </Button>
